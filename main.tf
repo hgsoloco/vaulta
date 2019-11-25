609608ac2c720a3cec3f9a14884024ba4a097b81
Daniel He <daniel.he@33740-mbpr.2d.hq>
2019-11-25 18:48:48 -0500

    f

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
-  region = us-east-1
-  access_key = "${var.vault_access_key}"
-  secret_key = "${var.vault_secret_key}"
+  region = "us-east-1"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
 }
 
 resource "aws_instance" "web-app" {
