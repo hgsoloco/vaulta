f254982a5c5e49f7f3232bb971d06b8377dba5db
Daniel He <daniel.he@33740-mbpr.2d.hq>
2019-11-25 17:30:41 -0500

    hi

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "web-app" {
-  ami             = "ami-3eee4150"
+  ami             = "ami-00068cd7555f543d5"
   instance_type   = "t2.micro"
   associate_public_ip_address = true
   subnet_id = "subnet-c5c3059d"
