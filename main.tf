136cf965c1cfab0ded4ac2884c6b1353758929d4
hgsoloco <onlythemaple@gmail.com>
2019-11-25 20:40:35 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,7 +1,8 @@
 provider "aws" {
-  access_key = "xxx"
-  secret_key = "xxx"
-  region     = "us-east-1"}
+  region     = "us-east-1"
+  access_key = "${var.access_key}"
+  secret_key = "${var.secret_key}"
+}
 
 resource "aws_instance" "web-app" {
   ami             = "ami-00068cd7555f543d5"
