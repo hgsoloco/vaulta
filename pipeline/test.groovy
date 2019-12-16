62d0767b5af7a29e618f9dafca047f6aa3c600f8
Daniel He <daniel.he@33740-mbpr.fios-router.home>
2019-12-16 16:30:03 -0500

    updated ipa

--- pipeline/test.groovy
+++ pipeline/test.groovy
@@ -2,6 +2,9 @@ node {
 
   stage('Preparation') {
     sh 'pwd'
+    sh 'pwd'
+    sh 'ls'
+    sh 'pwd'
     properties = readProperties file: 'scripts/jenkins-pipelines/branch-specific.properties'
     echo "Running build ${JOB_NAME}"
 
