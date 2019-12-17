adf30b769b5fb27a0eeb6928520e8774998eaef1
Daniel He <daniel.he@33740-mbpr.fios-router.home>
2019-12-17 12:16:47 -0500

    updated ipa

--- pipeline/pipeline2.groovy
+++ pipeline/pipeline2.groovy
@@ -6,38 +6,11 @@ node {
 
   }
   stage('Build') {
+    sh 'curl --header "X-Vault-Token: s.0luSVWmp77ReJpy2VwcAxD3T" \
+    --request GET http://35.232.41.214:8200/aws/creds/s3-ec2 | jq
     
-    environment = "prod"
-      
-    def nonprodsecrets = [
-        [path: 'aws/creds/s3-ec2', engineVersion: 1, secretValues: [
-            [envVar: 'akey', vaultKey: 'access_key'],
-            [envVar: 'asecret', vaultKey: 'secret_key']]]
-    ]
 
-    def nonprodconfiguration = [vaultUrl: 'http://35.232.41.214:8200',
-                        vaultCredentialId: 'genny',
-                        engineVersion: 1]
     
-    def prodsecrets = [
-        [path: 'aws2/creds/prodaccount', engineVersion: 1, secretValues: [
-            [envVar: 'akey', vaultKey: 'access_key'],
-            [envVar: 'asecret', vaultKey: 'secret_key']]]
-    ]
-
-    def prodconfiguration = [vaultUrl: 'http://35.232.41.214:8200',
-                        vaultCredentialId: 'james',
-                        engineVersion: 1]  
-
-    echo "Environment Deployment to ${properties.environment}"
-
-    withVault([configuration: nonprodconfiguration, vaultSecrets: nonprodsecrets]) {
-            sh 'chmod +x ./delay-vault-aws.sh && ./delay-vault-aws.sh'
-            sh 'terraform init'
-            sh 'terraform plan -var secret_key=$asecret -var access_key=$akey'
-            sh 'terraform apply -var secret_key=$asecret -var access_key=$akey -auto-approve'
-            sh 'terraform destroy -var secret_key=$asecret -var access_key=$akey -force'
-   }
     }
     
   }
