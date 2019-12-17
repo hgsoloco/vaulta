ec1b9e0decf9742a8407dded496daa30728909a9
rvecwxqz948 <dramamuse1@gmail.com>
2019-12-17 15:51:39 -0500

    hi

--- pipeline/pipeline2.groovy
+++ pipeline/pipeline2.groovy
@@ -10,6 +10,7 @@ node {
     ak=$(curl --header "X-Vault-Token: s.0luSVWmp77ReJpy2VwcAxD3T" \
     --request GET http://35.232.41.214:8200/v1/aws/creds/s3-ec2) | jq
     '''
+    sh 'echo $ak'
 
     
     }
