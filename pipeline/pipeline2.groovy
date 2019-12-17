50b4fc176f12b9050f526d52997dbe98db7c2197
rvecwxqz948 <dramamuse1@gmail.com>
2019-12-17 16:19:13 -0500

    hi

--- pipeline/pipeline2.groovy
+++ pipeline/pipeline2.groovy
@@ -7,7 +7,7 @@ node {
   }
   stage('Build') {
     sh '''#!/bin/bash
-    k=$(curl --header "X-Vault-Token: s.0luSVWmp77ReJpy2VwcAxD3T" \
+    k=$(curl --header "X-Vault-Token: ${properties.token}" \
     --request GET http://35.232.41.214:8200/v1/aws/creds/s3-ec2 | jq -r '.data.access_key,.data.secret_key')
     ak=$(echo $k | cut -d ' ' -f 1) && sk=$(echo $k | cut -d ' ' -f 2)
     chmod +x ./delay-vault-aws.sh && ./delay-vault-aws.sh
@@ -22,7 +22,7 @@ node {
     
 }
 
-
+s.0luSVWmp77ReJpy2VwcAxD3T
 // bk=$(echo $ak | cut -d ' ' -f 1) && sk=$(echo $ak | cut -d ' ' -f 2)
 // ak=$( curl --header "X-Vault-Token: s.0luSVWmp77ReJpy2VwcAxD3T" 
 // --request GET http://35.232.41.214:8200/v1/aws/creds/s3-ec2 | jq -r '.data.access_key,.data.secret_key')
