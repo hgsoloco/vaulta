c9c0a655a561a0016afb6cc7508ce1201d0da4fb
rvecwxqz948 <dramamuse1@gmail.com>
2019-12-17 15:25:25 -0500

    hi

--- pipeline/pipeline2.groovy
+++ pipeline/pipeline2.groovy
@@ -6,11 +6,14 @@ node {
 
   }
   stage('Build') {
-    sh 'curl --header "X-Vault-Token: s.0luSVWmp77ReJpy2VwcAxD3T" \
-    --request GET http://35.232.41.214:8200/v1/aws/creds/s3-ec2'
+    sh 'ak=$( curl --header "X-Vault-Token: s.0luSVWmp77ReJpy2VwcAxD3T" \
+    --request GET http://35.232.41.214:8200/v1/aws/creds/s3-ec2 | jq -r '.data.access_key,.data.secret_key')'
     
 
     
     }
     
 }
+
+
+// bk=$(echo $ak | cut -d ' ' -f 1) && sk=$(echo $ak | cut -d ' ' -f 2)
