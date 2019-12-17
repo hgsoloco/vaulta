e797f7d80a2283f77622c85bd2be281eba615d79
rvecwxqz948 <dramamuse1@gmail.com>
2019-12-17 18:20:19 -0500

    hi

--- pipeline/pipeline2.groovy
+++ pipeline/pipeline2.groovy
@@ -9,9 +9,9 @@ node {
     p = 100
     
     sh "echo 'token is ${properties.token}'"
-    sh "k=\$(curl --header \"X-Vault-Token: ${properties.token}\" \
+    sh "p=\$(curl --header \"X-Vault-Token: ${properties.token}\" \
     --request GET http://35.232.41.214:8200/v1/aws/creds/s3-ec2 | jq -r '.data.access_key,.data.secret_key')"
-    sh "echo $k"
+    sh "echo $p"
     sh """
     echo 'value is ${properties.token}'
     
