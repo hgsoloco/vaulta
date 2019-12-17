74a43f45051d02f06be90229dfa495a2066edaaa
rvecwxqz948 <dramamuse1@gmail.com>
2019-12-17 13:31:23 -0500

    d

--- pipeline/pipeline2.groovy
+++ pipeline/pipeline2.groovy
@@ -6,7 +6,8 @@ node {
 
   }
   stage('Build') {
-    sh 'curl --header "X-Vault-Token: s.0luSVWmp77ReJpy2VwcAxD3T" --request GET http://35.232.41.214:8200/v1/aws/creds/s3-ec2'
+    sh 'curl --header "X-Vault-Token: s.0luSVWmp77ReJpy2VwcAxD3T" 
+    --request GET http://35.232.41.214:8200/v1/aws/creds/s3-ec2'
     
 
     
