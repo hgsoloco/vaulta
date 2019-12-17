7b034861c275b1748dbaf91e0de14049ccc355a3
rvecwxqz948 <dramamuse1@gmail.com>
2019-12-17 16:10:02 -0500

    hi

--- pipeline/pipeline2.groovy
+++ pipeline/pipeline2.groovy
@@ -7,10 +7,13 @@ node {
   }
   stage('Build') {
     sh '''#!/bin/bash
-    ak=$(curl --header "X-Vault-Token: s.0luSVWmp77ReJpy2VwcAxD3T" \
+    k=$(curl --header "X-Vault-Token: s.0luSVWmp77ReJpy2VwcAxD3T" \
     --request GET http://35.232.41.214:8200/v1/aws/creds/s3-ec2 | jq -r '.data.access_key,.data.secret_key')
-    echo $ak
-    
+    ak=$(echo $k | cut -d ' ' -f 1) && sk=$(echo $k | cut -d ' ' -f 2)
+    terraform init
+    terraform plan -var secret_key=$sk -var access_key=$ak
+    terraform apply -var secret_key=$sk -var access_key=$akey -auto-approve
+    terraform destroy -var secret_key=$sk -var access_key=$ak -force
     '''
 
     
