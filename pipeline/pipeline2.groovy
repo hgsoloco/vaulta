28c9175f612bd927f7a46a04f62e718e725cd25b
rvecwxqz948 <dramamuse1@gmail.com>
2019-12-17 16:11:22 -0500

    hi

--- pipeline/pipeline2.groovy
+++ pipeline/pipeline2.groovy
@@ -10,6 +10,10 @@ node {
     k=$(curl --header "X-Vault-Token: s.0luSVWmp77ReJpy2VwcAxD3T" \
     --request GET http://35.232.41.214:8200/v1/aws/creds/s3-ec2 | jq -r '.data.access_key,.data.secret_key')
     ak=$(echo $k | cut -d ' ' -f 1) && sk=$(echo $k | cut -d ' ' -f 2)
+    echo $k
+    echo $ak
+    echo $sk
+    chmod +x ./delay-vault-aws.sh && ./delay-vault-aws.sh'
     terraform init
     terraform plan -var secret_key=$sk -var access_key=$ak
     terraform apply -var secret_key=$sk -var access_key=$akey -auto-approve
