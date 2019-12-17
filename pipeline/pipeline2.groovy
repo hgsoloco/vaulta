b29ba92b5685bd74ec46c23cd038b2cd512d8f88
rvecwxqz948 <dramamuse1@gmail.com>
2019-12-17 16:53:00 -0500

    hi

--- pipeline/pipeline2.groovy
+++ pipeline/pipeline2.groovy
@@ -9,6 +9,9 @@ node {
     sh """
     k=\$(curl --header "X-Vault-Token: ${properties.token}" \
     --request GET http://35.232.41.214:8200/v1/aws/creds/s3-ec2 | jq -r '.data.access_key,.data.secret_key')
+    ak=\$(echo $k | cut -d ' ' -f 1) && sk=$(echo $k | cut -d ' ' -f 2)
+    
+    
     """
     //sh 'k=$(curl --header "X-Vault-Token: $token" \
     //--request GET http://35.232.41.214:8200/v1/aws/creds/s3-ec2 | jq -r '.data.access_key,.data.secret_key')
