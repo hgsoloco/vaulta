6634509b2f106f7aa95f63da04fbbc62c561579f
rvecwxqz948 <dramamuse1@gmail.com>
2019-12-17 16:05:13 -0500

    hi

--- pipeline/pipeline2.groovy
+++ pipeline/pipeline2.groovy
@@ -9,9 +9,9 @@ node {
     sh '''#!/bin/bash
     ak=$(curl --header "X-Vault-Token: s.0luSVWmp77ReJpy2VwcAxD3T" \
     --request GET http://35.232.41.214:8200/v1/aws/creds/s3-ec2 | jq -r '.data.access_key,.data.secret_key')
+    echo $ak
+    
     '''
-    sh 'touch really.txt'
-    sh 'echo ${ak}'
 
     
     }
