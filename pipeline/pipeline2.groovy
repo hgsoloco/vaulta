1862c2414a853ab16df5e8b1b92dcd61294000de
rvecwxqz948 <dramamuse1@gmail.com>
2019-12-17 19:11:21 -0500

    hi

--- pipeline/pipeline2.groovy
+++ pipeline/pipeline2.groovy
@@ -7,17 +7,17 @@ node {
   }
   stage('Build') {
       env.some_var = "${properties.token}"
-      sh "echo \${some_var}"
-      //sh '''#!/bin/bash
-      //k=$(curl --header "X-Vault-Token: ${properties.token}" \
-      //--request GET http://35.232.41.214:8200/v1/aws/creds/s3-ec2 | jq -r '.data.access_key,.data.secret_key')
-      //ak=$(echo $k | cut -d ' ' -f 1) && sk=$(echo $k | cut -d ' ' -f 2)
-      //chmod +x ./delay-vault-aws.sh && ./delay-vault-aws.sh
-      //terraform init
-      //terraform plan -var secret_key=$sk -var access_key=$ak
-      //terraform apply -var secret_key=$sk -var access_key=$ak -auto-approve
-      //terraform destroy -var secret_key=$sk -var access_key=$ak -force
-      //'''
+      
+      sh '''#!/bin/bash
+      k=$(curl --header "X-Vault-Token: \${some_var}" \
+      --request GET http://35.232.41.214:8200/v1/aws/creds/s3-ec2 | jq -r '.data.access_key,.data.secret_key')
+      ak=$(echo $k | cut -d ' ' -f 1) && sk=$(echo $k | cut -d ' ' -f 2)
+      chmod +x ./delay-vault-aws.sh && ./delay-vault-aws.sh
+      terraform init
+      terraform plan -var secret_key=$sk -var access_key=$ak
+      terraform apply -var secret_key=$sk -var access_key=$ak -auto-approve
+      terraform destroy -var secret_key=$sk -var access_key=$ak -force
+      '''
     
 
 
@@ -25,6 +25,7 @@ node {
     }
     
 }
+// sh "echo \${some_var}"
 // ak=$(echo ${k} | cut -d ' ' -f 1) && sk=$(echo ${k} | cut -d ' ' -f 2)
 // s.0luSVWmp77ReJpy2VwcAxD3T
 // bk=$(echo $ak | cut -d ' ' -f 1) && sk=$(echo $ak | cut -d ' ' -f 2)
