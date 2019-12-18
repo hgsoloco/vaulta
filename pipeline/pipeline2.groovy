3c077deddfbb098b11bd85fd1facb0941b47578d
rvecwxqz948 <dramamuse1@gmail.com>
2019-12-17 19:37:49 -0500

    hi

--- pipeline/pipeline2.groovy
+++ pipeline/pipeline2.groovy
@@ -1,5 +1,5 @@
 node {
-  properties = readProperties file: 'pipeline/vars.properties'
+  loadEnvironmentVariables('pipeline/vars.properties')
   echo "Building Terraform in ${properties.environment} environment"
   stage('Preparation') {
     git 'https://github.com/hgsoloco/vaulta.git'
@@ -7,10 +7,8 @@ node {
   }
   stage('Build') {
       
-      env.some_var = "${properties.token}"
-      
       sh '''#!/bin/bash
-      k=$(curl --header "X-Vault-Token: ${properties.token}" \
+      k=$(curl --header "X-Vault-Token: ${token}" \
       --request GET http://35.232.41.214:8200/v1/aws/creds/s3-ec2 | jq -r '.data.access_key,.data.secret_key')
       ak=$(echo $k | cut -d ' ' -f 1) && sk=$(echo $k | cut -d ' ' -f 2)
       chmod +x ./delay-vault-aws.sh && ./delay-vault-aws.sh
@@ -20,13 +18,22 @@ node {
       terraform destroy -var secret_key=$sk -var access_key=$ak -force
       '''
 
-
+      
     
 
 
     
     }
-    
+
+
+    def loadEnvironmentVariables(path){
+      def props = readProperties  file: path
+      keys= props.keySet()
+      for(key in keys) {
+          value = props["${key}"]
+          env."${key}" = "${value}"
+      }
+    } 
 }
 // sh "echo \${some_var}"
 // ak=$(echo ${k} | cut -d ' ' -f 1) && sk=$(echo ${k} | cut -d ' ' -f 2)
