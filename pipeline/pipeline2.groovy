dc0a8dd15d2837addfbb61fd46d636f8cd229978
rvecwxqz948 <dramamuse1@gmail.com>
2020-01-02 13:30:55 -0500

    test

--- pipeline/pipeline2.groovy
+++ pipeline/pipeline2.groovy
@@ -12,19 +12,45 @@ node {
     git 'https://github.com/hgsoloco/vaulta.git'
   }
 
+  if ("${environment}" == 'nonprod') {
   stage('Build') {
-    sh '''#!/bin/bash
-      k=$(curl --header "X-Vault-Token: ${token}" \
-      --request GET http://35.239.60.86:8200/v1/aws/creds/s3-ec2 | jq -r '.data.access_key,.data.secret_key')
-      ak=$(echo $k | cut -d ' ' -f 1) && sk=$(echo $k | cut -d ' ' -f 2)
-      chmod +x ./delay-vault-aws.sh && ./delay-vault-aws.sh
-      terraform init
-      terraform plan -var secret_key=$sk -var access_key=$ak
-      terraform apply -var secret_key=$sk -var access_key=$ak -auto-approve
-      terraform destroy -var secret_key=$sk -var access_key=$ak -force
-    '''
+      sh '''#!/bin/bash
+        k=$(curl --header "X-Vault-Token: ${token}" \
+        --request GET http://35.239.60.86:8200/v1/aws/creds/s3-ec2 | jq -r '.data.access_key,.data.secret_key')
+        ak=$(echo $k | cut -d ' ' -f 1) && sk=$(echo $k | cut -d ' ' -f 2)
+        chmod +x ./delay-vault-aws.sh && ./delay-vault-aws.sh
+        terraform init
+        terraform plan -var secret_key=$sk -var access_key=$ak
+        terraform apply -var secret_key=$sk -var access_key=$ak -auto-approve
+        terraform destroy -var secret_key=$sk -var access_key=$ak -force
+      '''
+      }
   }  
+
+  if ("${environment}" == 'prod') {
+    stage('Build') {
+      sh '''#!/bin/bash
+        k=$(curl --header "X-Vault-Token: ${token}" \
+        --request GET http://35.239.60.86:8200/v1/aws/creds/prodaccount | jq -r '.data.access_key,.data.secret_key')
+        ak=$(echo $k | cut -d ' ' -f 1) && sk=$(echo $k | cut -d ' ' -f 2)
+        chmod +x ./delay-vault-aws.sh && ./delay-vault-aws.sh
+        terraform init
+        terraform plan -var secret_key=$sk -var access_key=$ak
+        terraform apply -var secret_key=$sk -var access_key=$ak -auto-approve
+        terraform destroy -var secret_key=$sk -var access_key=$ak -force
+      '''
+    }
+  }  
+
 }
+
+
+
+
+
+
+
+
 // sh "echo \${some_var}"
 // ak=$(echo ${k} | cut -d ' ' -f 1) && sk=$(echo ${k} | cut -d ' ' -f 2)
 // s.0luSVWmp77ReJpy2VwcAxD3T
