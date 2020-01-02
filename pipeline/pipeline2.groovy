e29a287cb6e5c5eba4fbc114491b0bc42e75e12f
rvecwxqz948 <dramamuse1@gmail.com>
2020-01-02 13:32:45 -0500

    test

--- pipeline/pipeline2.groovy
+++ pipeline/pipeline2.groovy
@@ -13,7 +13,7 @@ node {
   }
 
   if ("${environment}" == 'nonprod') {
-  stage('Build') {
+    stage('Build') {
       sh '''#!/bin/bash
         k=$(curl --header "X-Vault-Token: ${token}" \
         --request GET http://35.239.60.86:8200/v1/aws/creds/s3-ec2 | jq -r '.data.access_key,.data.secret_key')
@@ -39,7 +39,7 @@ node {
         terraform apply -var secret_key=$sk -var access_key=$ak -auto-approve
         terraform destroy -var secret_key=$sk -var access_key=$ak -force
       '''
-    }
+      }
   }  
 
 }
